0d0f114bdd82c972739f5d2fae79bbb40eb96c12
Chris24305 <christian.schaa@web.de>
2025-12-17 16:00:48 +0100

    H2V2

--- src/h2/Bus.java
+++ src/h2/Bus.java
@@ -1,5 +1,71 @@
 package h2;
 
-public class Bus {
+import java.util.ArrayList;
 
+public class Bus {
+	private ArrayList<Passenger> passengers;
+
+	public Bus() {
+		passengers = new ArrayList<>();
+	}
+
+	public void enterBus(Passenger p) {
+		passengers.add(p);
+	}
+
+	private void exitBus() {
+		if (passengers.size() != 0) {
+			Passenger mustExit;
+			for (int i = 0; i < passengers.size(); i++) {
+				mustExit = passengers.get(i);
+				if (mustExit.getPlanned() == mustExit.getVisited()) {
+					passengers.remove(i);
+				}
+			}
+		}
+	}
+
+	public void nextStop(Passenger[] boarding) {
+		for (int i = 0; i < passengers.size(); i++) {
+			passengers.get(i).increaseVisited();
+		}
+
+		exitBus();
+
+		for (int i = 0; i < boarding.length; i++) {
+			passengers.add(boarding[i]);
+		}
+	}
+
+	public void nextStop() {
+		for (int i = 0; i < passengers.size(); i++) {
+			passengers.get(i).increaseVisited();
+		}
+
+		exitBus();
+	}
+
+	public ArrayList<Passenger> findPassengersWithoutTickets() {
+		ArrayList<Passenger> noTicket = new ArrayList<>();
+		for (int i = 0; i < passengers.size(); i++) {
+			if (!passengers.get(i).getTicket()) {
+				noTicket.add(passengers.get(i));
+				passengers.remove(i);
+				i--;
+			}
+		}
+		return noTicket;
+	}
+
+	public void transferPassengers(Bus otherBus, String[] passengerNames) {
+		for (int i = 0; i < passengerNames.length; i++) {
+			for (int j = 0; j < passengers.size(); j++) {
+				if (passengerNames[i] == passengers.get(j).getName()) {
+					otherBus.enterBus(passengers.get(j));
+					passengers.remove(j);
+					break;
+				}
+			}
+		}
+	}
 }
--- src/h2/Passenger.java
+++ src/h2/Passenger.java
@@ -1,5 +1,35 @@
 package h2;
 
 public class Passenger {
+	private String name;
+	private int planned; // always >1, constant
+	private int visited; // if visited = planned Passenger arrives
+	private boolean ticket;
 
+	public Passenger(String name, int planned, boolean ticket) {
+		this.name = name;
+		this.planned = planned;
+		this.ticket = ticket;
+		visited = 0;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public int getPlanned() {
+		return this.planned;
+	}
+
+	public int getVisited() {
+		return this.visited;
+	}
+
+	public boolean getTicket() {
+		return this.ticket;
+	}
+
+	public void increaseVisited() {
+		this.visited++;
+	}
 }
